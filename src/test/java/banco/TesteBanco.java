888304930373a6426989b9eb9beef6e7e1615ce6
Gustavo Paco <gustavopaco@gmail.com>
2021-05-13 19:02:22 -0300

    JUnit de teste para o Banco de Dados

---
+++ src/test/java/banco/TesteBanco.java
@@ -0,0 +1,16 @@
+package banco;
+
+import org.junit.Test;
+
+import connection.SingleConnection;
+
+public class TesteBanco {
+
+	@Test
+	public void TestBanco() {
+		
+		new SingleConnection();
+		
+	}
+	
+}
