9845d7aebf8e1cf7987af62986de761104e4fdd9
Gustavo Paco <gustavopaco@gmail.com>
2021-05-15 03:46:08 -0300

    Implementando Model + DAO com metodos no Banco

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -17,79 +17,104 @@ import connection.SingleConnection;
 
 public class UsuarioDAO {
 
-		private Connection connection;
-	
-	public UsuarioDAO() {
-		connection = SingleConnection.getConnection();
-	}
-	
-	
-	public Boolean validaLogin(String login, String password) {
-		
-		try {
-			
-			String sql = " select login,password from usuario "
-					+ " where login = ? "
-					+ " and password = ? ";
-			
-			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, login);
-			preparedStatement.setString(2, password);
-			
-			ResultSet resultSet = preparedStatement.executeQuery();
-			
-			if(resultSet.next()) {
-				return true;
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-		
-	}
-	
-	public void cadastrarUsuario(Usuario usuario) throws Exception{
-		
-		try {
-			
-			String sql = "insert into usuario (login,password) values (?,?)";
-			PreparedStatement insertPreparedStatement = connection.prepareStatement(sql);
-			
-			insertPreparedStatement.setString(1, usuario.getLogin());
-			insertPreparedStatement.setString(2, usuario.getPassword());
-			
-			insertPreparedStatement.executeUpdate();
-			connection.commit();
-			
-		} catch (Exception e) {
-			try {
-				connection.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-			throw new Exception("Usuario nao foi registrado: \n" + e.getLocalizedMessage());
-		}
-		
-	}
+    private Connection connection;
 
-	public List<Usuario> listarUsuarios(){
-
-		ArrayList<Usuario> usuarios = new ArrayList<>();
-
-		try {
-
-			String sql = "select * from usuario";
-			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			ResultSet resultSet = preparedStatement.executeQuery();
-
-			
+    public UsuarioDAO() {
+       connection = SingleConnection.getConnection();
+    }
 
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-			return usuarios;
-	}
+    public Boolean validaLogin(String login, String password) {
+
+        try {
+
+            String sql = " select login,password from usuario "
+                    + " where login = ? "
+                    + " and password = ? ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    public void cadastrarUsuario(Usuario usuario) throws Exception {
+
+        try {
+
+            String sql = "insert into usuario (login,password) values (?,?)";
+            PreparedStatement insertPreparedStatement = connection.prepareStatement(sql);
+
+            insertPreparedStatement.setString(1, usuario.getLogin());
+            insertPreparedStatement.setString(2, usuario.getPassword());
+
+            insertPreparedStatement.executeUpdate();
+            connection.commit();
+
+        } catch (Exception e) {
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+            throw new Exception("Usuario nao foi registrado: \n" + e.getLocalizedMessage());
+        }
+
+    }
+
+    public List<Usuario> listarUsuarios() {
+
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+
+        try {
+
+            String sql = "select * from usuario";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                Usuario usuario = new Usuario();
+                usuario.setId(resultSet.getLong("id"));
+                usuario.setLogin(resultSet.getString("login"));
+                usuario.setPassword(resultSet.getString("password"));
+
+                usuarios.add(usuario);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return usuarios;
+    }
+
+    public void deletarUsuario(Long id){
+
+        try {
+            String sql = "delete from usuario where id = " + id;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
 
 }
--- src/main/java/models/Usuario.java
+++ src/main/java/models/Usuario.java
@@ -1,65 +1,56 @@
 package models;
 
+import java.util.Objects;
+
 public class Usuario {
 
-	private String login;
-	private String password;
-	
-	public Boolean validaUsuario(String login, String password) {
-		if(login.equals("admin") && password.equals("admin")) {
-			return true;
-		}
-		return false;
-	}
-	
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((login == null) ? 0 : login.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		if (login == null) {
-			if (other.login != null)
-				return false;
-		} else if (!login.equals(other.login))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		return true;
-	}
-	
-	@Override
-	public String toString() {
-		return "Usuario [login=" + login + ", password=" + password + "]";
-	}
-	
-	
-	
+    private Long id;
+    private String login;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario)) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id) && Objects.equals(login, usuario.login) && Objects.equals(password, usuario.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
