1396ac2506af4f14be616f1741d720221ce00c52
Gustavo Paco <gustavopaco@gmail.com>
2021-05-15 03:46:52 -0300

    Implementando Cadastro Tela + Controller

--- src/main/java/controller/CadastroServletController.java
+++ src/main/java/controller/CadastroServletController.java
@@ -9,37 +9,46 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.Usuario;
 
-@WebServlet("/cadastro")
-public class CadastroServletController extends HttpServlet{
-	private static final long serialVersionUID = 1L;
+@WebServlet("/cadastroCtl")
+public class CadastroServletController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UsuarioDAO usuarioDAO;
+    private Usuario usuario;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doPost(req, resp);
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String login = req.getParameter("login");
-		String password = req.getParameter("password");
-		
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
 
-				try {
-					usuarioDAO.cadastrarUsuario(login, password);
-					System.out.println("Usuario Cadastrado com Sucesso.");
-					RequestDispatcher requestDispatcher = req.getRequestDispatcher("homeActivity.jsp");
-					requestDispatcher.forward(req, resp);
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-					RequestDispatcher requestDispatcher = req.getRequestDispatcher("cadastroActivity.jsp");
-					requestDispatcher.forward(req, resp);
-				}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
 
-		
-	}
-	
+        usuario = new Usuario();
+        usuario.setLogin(login);
+        usuario.setPassword(password);
+        usuarioDAO = new UsuarioDAO();
+
+        if (!login.isEmpty() && !password.isEmpty()) {
+
+            try {
+                usuarioDAO.cadastrarUsuario(usuario);
+                System.out.println("Usuario Cadastrado com Sucesso.");
+                req.setAttribute("usuarios", usuarioDAO.listarUsuarios());
+                RequestDispatcher requestDispatcher = req.getRequestDispatcher("homeActivity.jsp");
+                requestDispatcher.forward(req, resp);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                RequestDispatcher requestDispatcher = req.getRequestDispatcher("cadastroActivity.jsp");
+                requestDispatcher.forward(req, resp);
+            }
+        } else {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("cadastroActivity.jsp");
+            requestDispatcher.forward(req, resp);
+        }
+    }
 }
--- src/main/webapp/cadastroActivity.jsp
+++ src/main/webapp/cadastroActivity.jsp
@@ -1,21 +1,29 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+         pageEncoding="ISO-8859-1" isELIgnored="false" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+    <meta charset="ISO-8859-1">
+    <title>Insert title here</title>
 </head>
 <body>
-	<h2>Tela de Cadastro</h2>
-	<form action="cadastro" method="post">
-		<label>Login:</label>
-		<input type="text" id="login" name="login" />
-		<br>
-		<label>Password</label>
-		<input type="password" id="password" name="password"/>
-		<br>
-		<button type="submit">Cadastrar</button>
-	</form>
+<h2>Tela de Cadastro</h2>
+<form action="cadastroCtl" method="post">
+    <table>
+        <tr>
+            <td><label>Login:</label></td>
+            <td><input type="text" id="login" name="login"/></td>
+        </tr>
+        <tr>
+            <td><label>Password</label></td>
+            <td><input type="password" id="password" name="password"/></td>
+        </tr>
+        <tr>
+            <td>
+                <button type="submit">Cadastrar</button>
+            </td>
+        </tr>
+    </table>
+</form>
 </body>
 </html>
