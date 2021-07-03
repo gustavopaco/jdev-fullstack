388f4d3bce01b6bd5c434568ccf286c9a9f98487
Gustavo Paco <gustavopaco@gmail.com>
2021-07-03 01:03:49 -0300

    Singleton Hibernate util class Jakarta e Session

---
+++ src/main/java/util/HibernateJakartaEMUtil.java
@@ -0,0 +1,32 @@
+package util;
+
+//import jakarta.persistence.EntityManager;
+//import jakarta.persistence.EntityManagerFactory;
+//import jakarta.persistence.Persistence;
+
+public class HibernateJakartaEMUtil {
+//    public static EntityManagerFactory factory = null;
+//
+//    static {
+//        init();
+//    }
+//
+//    public HibernateJakartaEMUtil() {
+//        init();
+//    }
+//
+//    private static void init(){
+//
+//        try{
+//            if (factory == null){
+//                factory = Persistence.createEntityManagerFactory("mvn-hibernate-hibernateCFG");
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static EntityManager getEntityManager(){
+//        return factory.createEntityManager(); // Chama a conexao
+//    }
+}
---
+++ src/main/java/util/HibernateSessionUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+//import jakarta.persistence.EntityManager;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionUtil {
+//    public static SessionFactory factory = null;
+//
+//    static {
+//        init();
+//    }
+//
+//    public HibernateSessionUtil() {
+//        init();
+//    }
+//
+//    private static void init(){
+//
+//        try{
+//            if (factory == null){
+//                Configuration configuration = new Configuration().configure("META-INF/hibernate.cfg.xml");
+//                factory = configuration.buildSessionFactory();
+//                System.out.println("Leu o arquivo");
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static EntityManager getEntityManager(){
+//        return (EntityManager) factory.createEntityManager(); // Chama a conexao
+//    }
+}
