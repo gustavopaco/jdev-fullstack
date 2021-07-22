7b69ed8020f1953cd0ab7c5bf4948f8cbaa5e340
Gustavo Paco <gustavopaco@gmail.com>
2021-07-22 11:19:57 -0300

    Criando estrutura de projeto Spring Boot

---
+++ src/test/java/br/com/webmvnspringboot/WebMvnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webmvnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvnSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
