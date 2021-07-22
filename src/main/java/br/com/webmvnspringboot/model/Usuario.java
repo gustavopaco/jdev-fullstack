14b54973030b7090d3481ec15883c664b8863adb
Gustavo Paco <gustavopaco@gmail.com>
2021-07-22 11:52:44 -0300

    Criando modelo de Usuario

---
+++ src/main/java/br/com/webmvnspringboot/model/Usuario.java
@@ -0,0 +1,98 @@
+package br.com.webmvnspringboot.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Objects;
+
+@Table
+@Entity(name = "Usuario")
+public class Usuario implements Serializable {
+
+    @Id
+    @SequenceGenerator(name = "usuario_sequence", sequenceName = "usuario_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usuario_sequence")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "dob", nullable = false)
+    private LocalDate dob;
+
+    @Transient
+    private Integer age;
+
+    public Usuario() {
+    }
+
+    public Usuario(String name, LocalDate dob, Integer age) {
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Usuario(Long id, String name, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario usuario)) return false;
+
+        return id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
