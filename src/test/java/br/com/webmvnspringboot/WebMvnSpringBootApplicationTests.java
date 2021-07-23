0fc406c66a7ccb6f58800583dc8e6a2285bc4f35
Gustavo Paco <gustavopaco@gmail.com>
2021-07-23 14:54:15 -0300

    heroku commit

--- src/test/java/br/com/webmvnspringboot/WebMvnSpringBootApplicationTests.java
+++ src/test/java/br/com/webmvnspringboot/WebMvnSpringBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebMvnSpringBootApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+ //   void contextLoads() {
+//    }
 
 }
