7f2fdb31ce6864e8ab71b534211fda95cfc4efbd
Gustavo Paco <gustavopaco@gmail.com>
2021-08-11 15:19:39 -0300

    Commit Inicial Heroku

--- src/main/java/br/com/webmvnspringbootthymeleaf/util/RelatorioJDEV.java
+++ src/main/java/br/com/webmvnspringbootthymeleaf/util/RelatorioJDEV.java
@@ -5,21 +5,14 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
-import javax.servlet.ServletContext;
-import java.io.File;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class RelatorioJDEV implements Serializable {
-
-    @Autowired
-    private ResourceLoader resourceLoader;
 
     public byte[] gerarRelatorio(List<?> list) throws Exception {
         final InputStream in = this.getClass().getResourceAsStream("/relatorio.jrxml");
