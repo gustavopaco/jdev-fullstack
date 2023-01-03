a7f3c807b4d98023471aa8460eff5f9b4dd6ba78
Gustavo Paco <gustavopaco@gmail.com>
2023-01-03 13:01:48 -0300

    - Criando Repositorio - Populando banco com usuarios - CRUD Rest com retorno JSONs | String - Mapeamento de Controladores GET | POST | PUT | DELETE Trabalhando com Anotacoes de @Path Variable (direto na URL), @RequestParam (dados vindo de Formulario), @RequestBody (Objetos vindo via JSON)

--- src/main/java/com/pacoprojects/revjsbtapi/controller/Greetings.java
+++ src/main/java/com/pacoprojects/revjsbtapi/controller/Greetings.java
@@ -3,14 +3,18 @@ package com.pacoprojects.revjsbtapi.controller;
 import com.pacoprojects.revjsbtapi.model.Usuario;
 import com.pacoprojects.revjsbtapi.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping
+@RequestMapping(path = "sds")
 public class Greetings {
 
     private final UsuarioRepository usuarioRepository;
@@ -29,4 +33,63 @@ public class Greetings {
         return "Ola Mundo!";
     }
 
+    @GetMapping(path = "list")
+    public ResponseEntity<?> listarUsuarios() {
+        List<Usuario> usuarioList = usuarioRepository.findAll();
+        if (!usuarioList.isEmpty()) {
+            return ResponseEntity.ok(usuarioList);
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Nao contem usuarios cadastrados");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addUsuario(@RequestBody Usuario usuario, BindingResult bindingResult) {
+        try {
+            usuarioRepository.save(usuario);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erro ao cadastrar usuario");
+        }
+        return ResponseEntity.ok("Usuario cadastrado com sucesso");
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable Long id) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+        if (usuarioOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuario nao foi encontrado");
+        }
+        usuarioRepository.delete(usuarioOptional.get());
+        return ResponseEntity.ok("Usuario deletado com sucesso");
+    }
+
+    @GetMapping(path = "search")
+    public ResponseEntity<Usuario> findUsuarioById(@RequestParam(name = "userID") Long userID) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(userID);
+        if (usuarioOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuario Nao encontrado");
+        }
+        return ResponseEntity.ok(usuarioOptional.get());
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> updateUsuario(@RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(usuario.getId());
+
+        if (usuarioOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuario nao foi encontrado para atualizacao");
+        }
+        usuarioRepository.save(usuario);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping(path = "searchName")
+    public ResponseEntity<List<Usuario>> findByNamePart(@RequestParam(name = "namePart") String namePart) {
+        List<Usuario> usuarioList = usuarioRepository.findByNomeContainingIgnoreCase(namePart.strip());
+
+        if (usuarioList.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nao foi encontrado usuarios com esse nome.");
+        }
+        return ResponseEntity.ok(usuarioList);
+    }
 }
--- src/main/java/com/pacoprojects/revjsbtapi/repository/UsuarioRepository.java
+++ src/main/java/com/pacoprojects/revjsbtapi/repository/UsuarioRepository.java
@@ -4,7 +4,11 @@ import com.pacoprojects.revjsbtapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    List<Usuario> findByNomeContainingIgnoreCase(String nome);
 }
