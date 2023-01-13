4bbe5f2b6931fa212c2b4bba624e62138756d987
Gustavo Paco <gustavopaco@gmail.com>
2023-01-13 10:18:34 -0300

    - Criando Repository com metodo que sera usado pelo Service para buscar Usuario pelo username(login) e suas permissoes(roles)

--- src/main/java/com/pacoprojects/repository/UsuarioRepository.java
+++ src/main/java/com/pacoprojects/repository/UsuarioRepository.java
@@ -4,7 +4,11 @@ import com.pacoprojects.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findUsuarioByLogin(String login);
 
 }
