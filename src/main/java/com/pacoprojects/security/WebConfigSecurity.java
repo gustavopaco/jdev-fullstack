69677b68c08c98c0026374f7da777456605bc68f
Gustavo Paco <gustavopaco@gmail.com>
2023-01-25 12:08:21 -0300

    - Criando Class(WebSecurityConfig) com Configuracao do Spring Security.
    > Implementando gerenciador de Autenticacao(UserDetailsService) e Class(BCryptPasswordEncoder) de Criptografia de senhas
    
    > Implementando metodo de mapeamento de end-points e definindo privilegios. Adicionando Filtros de Requisicoes para URIs authenticated por Jwt

---
+++ src/main/java/com/pacoprojects/security/WebConfigSecurity.java
@@ -0,0 +1,53 @@
+package com.pacoprojects.security;
+
+import com.pacoprojects.service.ImpUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/* Mapeia URL, enderecos, autoriza ou bloqueia acesso a URL*/
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+    private final ImpUserDetailsService impUserDetailsService;
+
+    private final JWTFilter jwtFilter;
+
+    private final JWTAutenticacaoService jwtAutenticacaoService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /* Service que implementa a interface UserDetailsService, e seu metodo loadUserByUsername apos isso criptografa o password*/
+        auth.userDetailsService(impUserDetailsService)
+                /* Padrao de criptografia de senha */
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    /* Configura as solicitacoes de acesso Http */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /* Ativando a protecao contra usuarios que nao estao validados por token */
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).disable()
+                /* Ativando a permissao para acesso a pagina inicial do sistema sem precisar Logar */
+                .authorizeRequests().antMatchers("/", "/index").permitAll()
+                .antMatchers(HttpMethod.POST,"/usuario").permitAll()
+                /* Qualquer outra URL precisara de estar logado */
+                .anyRequest().authenticated()
+                /* URL de Logout - Redireciona apos o user deslogar do sistema para o index.html */
+                .and().logout().logoutSuccessUrl("/index")
+                /* Mapeia URL de Logout e invalida o usuario */
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                /* TODO filtra as demais requisicoes se contem a presenca do JWT no HEADER HTTP */
+                .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                /* TODO filtra as requisicoes de login para autenticacao */
+                .addFilter(new JWTLoginFilter(authenticationManager(),jwtAutenticacaoService));
+    }
+}
