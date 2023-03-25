c7e9a6a1007fbc3f529186121858502120b43d97
Gustavo Paco <gustavopaco@gmail.com>
2023-03-25 07:43:57 -0300

    Configuracao ApplicationConfig

--- src/main/java/com/pacoprojects/util/Constantes.java
+++ src/main/java/com/pacoprojects/util/Constantes.java
@@ -7,4 +7,5 @@ public class Constantes {
     public static final String AUTHORIZATION_SERVICE_EXCEPTION_MESSAGE = "Usuário sem permissão, por favor tente logar novamente.";
     public static final String EXPIRED_JWT_EXCEPTION_MESSAGE = "Sessão expirada, por favor faça o login novamente.";
     public static final String MALFORMED_JWT_EXCEPTION_MESSAGE = "Sessão foi invalidada, por favor faça o login novamente.";
+    public static final String USERNAME_NOTFOUND_EXCEPTION = "Usuário não existente.";
 }
